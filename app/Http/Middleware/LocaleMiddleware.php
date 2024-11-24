7f0ed1f07c57af3095b75735d1d2a4a635bde0b0
DoMAAA21 <clashecurry21@gmail.com>
2024-11-24 12:23:06 +0800

    feat: add locale

---
+++ app/Http/Middleware/LocaleMiddleware.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Inertia\Inertia;
+
+class LocaleMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $locale = session('locale',  config('locale.default'));
+        app()->setLocale($locale);
+        return $next($request);
+    }
+}
