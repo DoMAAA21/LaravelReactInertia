d4b4906064fda26ea3b1deced2b491f2418a62c1
DoMAAA21 <clashecurry21@gmail.com>
2024-11-24 11:39:30 +0800

    feat: add locale

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\App;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +22,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
+        $locale = session('locale', config('app.locale'));  // Default to 'en' or any default locale
+        App::setLocale($locale);
     }
 }
