ba410ba986ae746fb8df39d4aa26d7fbf3e22475
DoMAAA21 <clashecurry21@gmail.com>
2024-11-24 15:16:32 +0800

    feat: add theme

---
+++ resources/js/Components/Theme.ts
@@ -0,0 +1,9 @@
+export const colors = {
+    primary: 'bg-black text-gray-100 hover:bg-blue-700',
+    secondary: 'bg-indigo-500 text-gray-100 hover:bg-indigo-700',
+    warning: 'bg-yellow-700 text-white hover:bg-yellow-700',
+    dark: 'bg-gray-700 text-white hover:bg-gray-900',
+    info: 'bg-blue-400 text-white hover:bg-blue-700',
+    danger: 'bg-red-600 text-white hover:bg-red-800',
+    neutral: 'bg-white hover:bg-black hover:text-white border border-black'
+};
