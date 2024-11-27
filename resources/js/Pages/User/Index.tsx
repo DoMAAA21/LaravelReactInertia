94f69feb659d6facd0d831500ba82050d542a1d0
DoMAAA21 <clashecurry21@gmail.com>
2024-11-27 21:42:40 +0800

    todo: continue delete

--- resources/js/Pages/User/Index.tsx
+++ resources/js/Pages/User/Index.tsx
@@ -1,37 +1,74 @@
 import { useState, useEffect } from 'react';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import DataTable from '@/Components/data-table/DataTable';
+import { DataTable, DataTableActions } from '@/Components/data-table';
 import { UserListProps } from '@/types';
 import { router } from '@inertiajs/react';
-import useFetch from '@/hooks/useFetch';
+import { useFetch } from '@/hooks/useFetch';
+import ConfirmationPopUp from '@/Components/modal/ConfirmationPopUp';
+
 
 export default function UserList({ users, current_page, total_pages, total_rows, per_page, t }: UserListProps) {
     const [page, setPage] = useState(current_page);
+    const [userId, setUserId] = useState<null | string | number>(null);
     const [searchParams, setSearchParams] = useState({});
     const [withDeleted, setWithDeleted] = useState(false);
     const [rowsPerPage, setRowsPerPage] = useState(per_page);
 
     useFetch({ page, rowsPerPage, searchParams, withDeleted });
+    const onShow = (id: number | string) => {
 
+    }
+
+    const onEdit = (id: number | string) => {
+
+    }
+
+    const onDelete = (id: number | string) => {
+        const title = t.info.confirmDelete;
+        const message = t.info.areYouSure;
+        const confirmButton = t.button.confirm;
+        const cancelButton = t.button.cancel;
+        ConfirmationPopUp({
+            title,
+            message,
+            onSuccess: () => {
+
+            },
+            confirmButton,
+            cancelButton
+        })
+    }
+
+    const onRestore = (id: number | string) => {
+
+    }
     const columns = [
         { label: '#', field: 'id', sortField: 'id', width: '15%' },
-        { label: t.field.name, field: 'name', sortField: 'name', width: '20%' },
-        { label: t.field.email, field: 'email', sortField: 'email', width: '40%' },
+        { label: t.field.name, field: 'name', sortField: 'name', width: '35%' },
+        { label: t.field.email, field: 'email', sortField: 'email', width: '35%' },
         { label: t.field.actions, field: 'actions', width: '15%' },
     ]
 
     const rows = users?.map(user => {
-        const isDeleted = user.email_verified_at;
+        const isDeleted: boolean = user.deleted_at !== undefined && user.deleted_at !== null;
 
         return {
             id: user.id,
             name: user.name,
             email: user.email,
             actions: (
-                <>asa</>
-            )
+                <DataTableActions
+                    toggleId={user.id}
+                    toggleShow={onShow}
+                    toggleEdit={onEdit}
+                    toggleDelete={onDelete}
+                    toggleRestore={onRestore}
+                    isDeleted={isDeleted}
+                />
+            ),
+            isDeleted
         }
-    });
+    }) || [];
 
     const handleRowsPerPageChange = (perPage: number) => {
         setRowsPerPage(perPage);
@@ -55,6 +92,10 @@ export default function UserList({ users, current_page, total_pages, total_rows,
         }))
     }
 
+    const handleCheckboxChange = () => {
+        setWithDeleted(!withDeleted);
+    }
+
 
 
     return (
@@ -71,6 +112,9 @@ export default function UserList({ users, current_page, total_pages, total_rows,
                     onPageChange={handlePageChange}
                     onRowsPerPageChange={handleRowsPerPageChange}
                     onSortChange={handleSortChange}
+                    withDeleted={withDeleted}
+                    handleCheckboxChange={handleCheckboxChange}
+                    checkboxLabel={t.field.includeDeleted}
                 />
             </div>
 
